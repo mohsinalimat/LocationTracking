cf620adc090b39b1b1a33c52d7d1d7aa8154f39e
lighthouse2403 <haidang2403@gmail.com>
2017-06-21 00:54:21 +0700

    no message

---
+++ LocationTracking/LocationTracking/AddContactViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddContactViewController.swift
+//  LocationTracking
+//
+//  Created by Nguyen Hai Dang on 6/20/17.
+//  Copyright © 2017 Nguyen Hai Dang. All rights reserved.
+//
+
+import UIKit
+
+class AddContactViewController: OriginalViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
